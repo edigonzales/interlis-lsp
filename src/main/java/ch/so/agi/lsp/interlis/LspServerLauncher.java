cb4a3127e98b8d5e0403af3a77001143bca01b8c
Stefan Ziegler <edi.gonzales@gmail.com>
2025-10-22 10:50:26 +0200

    Start GLSP server from launcher

--- src/main/java/ch/so/agi/lsp/interlis/LspServerLauncher.java
+++ src/main/java/ch/so/agi/lsp/interlis/LspServerLauncher.java
@@ -8,6 +8,11 @@ import java.util.concurrent.ExecutionException;
 public class LspServerLauncher {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         InterlisLanguageServer server = new InterlisLanguageServer();
+        // Start the embedded GLSP endpoint as early as possible so clients can
+        // connect before the LSP handshake finishes. The initializer still
+        // guards startup for in-process tests that instantiate the server
+        // directly without going through this launcher.
+        server.getGlspServer().startAsync();
         
         Launcher<InterlisLanguageClient> launcher = Launcher.createLauncher(
                 server, InterlisLanguageClient.class, System.in, System.out);
