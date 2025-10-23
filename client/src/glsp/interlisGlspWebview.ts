7e6373351068662213754dcbb3dacccfd9a46996
Stefan Ziegler <edi.gonzales@gmail.com>
2025-10-23 11:10:27 +0200

    Fix container configuration import for GLSP webview

--- client/src/glsp/interlisGlspWebview.ts
+++ client/src/glsp/interlisGlspWebview.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
-import { Container, ContainerConfiguration } from 'inversify';
+import { Container } from 'inversify';
 import { GLSPStarter, VSCODE_DEFAULT_MODULE_CONFIG } from '@eclipse-glsp/vscode-integration-webview';
-import { initializeDiagramContainer } from '@eclipse-glsp/client';
+import { initializeDiagramContainer, type ContainerConfiguration } from '@eclipse-glsp/client';
 
 class InterlisGlspWebviewStarter extends GLSPStarter {
   protected createContainer(...containerConfiguration: ContainerConfiguration): Container {
